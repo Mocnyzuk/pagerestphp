8126846112bd4faa1b7579eed4b3b3b196f5b862
mocnyzuk <jakub.jureczko@interia.pl>
2020-06-17 01:16:53 +0200

    implement of redirecting and forwarding for SPA

--- src/Subscriber/ExceptionSubscriber.php
+++ src/Subscriber/ExceptionSubscriber.php
@@ -5,22 +5,15 @@ namespace App\Subscriber;
 
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
 
     public function onException(ExceptionEvent $event){
-        $e = $event->getThrowable();
-        if(!$e instanceof AccessDeniedHttpException){
-            return;
-        }
-        $responseData = [ "error" => $e->getMessage()];
-        $response = new JsonResponse($responseData);
-        $event->setResponse($response);
+        $event->setResponse(new RedirectResponse("/"));
     }
 
     public static function getSubscribedEvents()
